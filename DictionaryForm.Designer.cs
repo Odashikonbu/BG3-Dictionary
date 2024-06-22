54f8b897cc4b19c750b0659b39fcc4ae3f8ab56d
Odashikonbu <odackonb@gmail.com>
2024-06-22 22:02:20 +0900

    プロジェクト ファイルを追加します。

---
+++ DictionaryForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace BG3_Dictionary
+{
+    partial class Top
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Top));
+            this.Search_Box = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SerchResult = new System.Windows.Forms.DataGridView();
+            this.CreateDictionaryButton = new System.Windows.Forms.Button();
+            this.topBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.topBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.SerchResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Search_Box
+            // 
+            this.Search_Box.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Search_Box.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Search_Box.Location = new System.Drawing.Point(42, 32);
+            this.Search_Box.Name = "Search_Box";
+            this.Search_Box.Size = new System.Drawing.Size(561, 39);
+            this.Search_Box.TabIndex = 0;
+            this.Search_Box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SearchBox_KeyPress);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchButton.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.SearchButton.Location = new System.Drawing.Point(622, 32);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(140, 39);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // SerchResult
+            // 
+            this.SerchResult.AllowUserToAddRows = false;
+            this.SerchResult.AllowUserToDeleteRows = false;
+            this.SerchResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SerchResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SerchResult.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.SerchResult.Location = new System.Drawing.Point(42, 113);
+            this.SerchResult.Name = "SerchResult";
+            this.SerchResult.ReadOnly = true;
+            this.SerchResult.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.SerchResult.RowTemplate.Height = 21;
+            this.SerchResult.Size = new System.Drawing.Size(720, 316);
+            this.SerchResult.TabIndex = 2;
+            // 
+            // CreateDictionaryButton
+            // 
+            this.CreateDictionaryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CreateDictionaryButton.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CreateDictionaryButton.Location = new System.Drawing.Point(42, 454);
+            this.CreateDictionaryButton.Name = "CreateDictionaryButton";
+            this.CreateDictionaryButton.Size = new System.Drawing.Size(184, 39);
+            this.CreateDictionaryButton.TabIndex = 1;
+            this.CreateDictionaryButton.Text = "Create Dictionary";
+            this.CreateDictionaryButton.UseVisualStyleBackColor = true;
+            this.CreateDictionaryButton.Click += new System.EventHandler(this.CreateDictionaryButton_Click);
+            // 
+            // topBindingSource
+            // 
+            this.topBindingSource.DataSource = typeof(BG3_Dictionary.Top);
+            // 
+            // topBindingSource1
+            // 
+            this.topBindingSource1.DataSource = typeof(BG3_Dictionary.Top);
+            // 
+            // Top
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 505);
+            this.Controls.Add(this.SerchResult);
+            this.Controls.Add(this.CreateDictionaryButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.Search_Box);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(816, 544);
+            this.Name = "Top";
+            this.Text = "BG3 Language Dictionary";
+            this.Load += new System.EventHandler(this.Top_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SerchResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Search_Box;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.DataGridView SerchResult;
+        private System.Windows.Forms.Button CreateDictionaryButton;
+        private System.Windows.Forms.BindingSource topBindingSource;
+        private System.Windows.Forms.BindingSource topBindingSource1;
+    }
+}
+
---
+++ DictionaryForm.cs
@@ -0,0 +1,250 @@
+﻿namespace BG3_Dictionary
+{
+    using LiteDB;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+    using System.Xml.Linq;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+    public struct TranslationData
+    {
+        public ObjectId ID { get; set; }
+        public string UUID { get; set; }
+        public string SourceLang { get; set; }
+        public string TransLang { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the <see cref="Top" />
+    /// </summary>
+    public partial class Top : Form
+    {
+        private string DBPath = "translation.db";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Top"/> class.
+        /// </summary>
+        public Top()
+        {
+            InitializeComponent();
+
+            //DataGridView幅設定
+            SetupDataGridView();
+        }
+        private void SetupDataGridView()
+        {
+            SerchResult.AutoGenerateColumns = false;
+            SerchResult.AllowUserToAddRows = false;
+            SerchResult.Columns.Clear();
+
+            // 行の高さ1.5倍に設定
+            SerchResult.RowTemplate.Height = (int)(SerchResult.RowTemplate.Height * 1.5);
+            SerchResult.RowHeadersWidth = 10;
+
+            int hi = SerchResult.Height;
+            int ch = SerchResult.ColumnHeadersHeight;
+            int h = SerchResult.RowTemplate.Height;
+            int n = (hi - ch) / h;
+            hi = ch + h * n + 2;
+
+            SerchResult.Height = hi;
+
+
+            // UUID
+            DataGridViewTextBoxColumn uuidColumn = new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "UUID",
+                HeaderText = "uuid",
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+                FillWeight = 10 // 比率 1
+            };
+            SerchResult.Columns.Add(uuidColumn);
+
+            // 原文
+            DataGridViewTextBoxColumn sourceLangColumn = new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "SourceLang",
+                HeaderText = "原文",
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+                FillWeight = 30 // 比率 2
+            };
+            SerchResult.Columns.Add(sourceLangColumn);
+
+            // 訳文
+            DataGridViewTextBoxColumn transLangColumn = new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "TransLang",
+                HeaderText = "訳文",
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+                FillWeight = 60 // 比率 2
+            };
+            SerchResult.Columns.Add(transLangColumn);
+
+            // 自動的にカラム幅を調整するように設定
+            SerchResult.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+        /// <summary>
+        /// The Top_Load
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
+        private void Top_Load(object sender, EventArgs e)
+        {
+
+            using (var db = new LiteDatabase(DBPath))
+            {
+                var collection = db.GetCollection<TranslationData>("TranslationData");
+                // インデックス付ける
+                collection.EnsureIndex(x => x.ID);
+
+                var data = collection.Find(Query.All(), limit: 20).ToList();
+
+                if (data.Count > 0)
+                {
+                    SerchResult.DataSource = data;
+                    SerchResult.Visible = data.Count >= 20;
+                }
+                else
+                {
+                    SerchResult.Visible = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The SearchBox_KeyPress
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="KeyPressEventArgs"/></param>
+        private void SearchBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                SearchButton_Click(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string searchText = Search_Box.Text.Trim();
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                Search_Box.Enabled = false;
+                SearchButton.Enabled = false;
+                using (var db = new LiteDatabase(DBPath))
+                {
+                    var collection = db.GetCollection<TranslationData>("TranslationData");
+                    var result = collection.Find(x => x.SourceLang.Contains(searchText)).Select(x => new TranslationData
+                    {
+                        UUID = x.UUID,
+                        SourceLang = x.SourceLang,
+                        TransLang = x.TransLang
+                    }).ToList(); ;
+
+                    if (result.Count > 0)
+                    {
+                        SetupDataGridView();
+                        SerchResult.DataSource = result;
+                        SerchResult.Visible = true;
+                    }
+                    else
+                    {
+                        SerchResult.Visible = false;
+                    }
+
+                }
+                Search_Box.Enabled = true;
+                SearchButton.Enabled = true;
+            }
+               
+        }
+
+
+
+        private void CreateDictionaryButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "XML files (*.xml)|*.xml",
+                    Title = "英語XMLファイルを選択してください"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string sourceXML = openFileDialog.FileName;
+
+                    openFileDialog.Reset();
+                    openFileDialog.Title = "日本語XMLファイルを選択してください";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string transXML = openFileDialog.FileName;
+
+                        // XML読み込み
+                        XDocument sourceDoc = XDocument.Load(sourceXML);
+                        XDocument translationDoc = XDocument.Load(transXML);
+
+                        // XML辞書化
+                        var sourceSentences = sourceDoc.Descendants("content")
+                            .ToDictionary(x => (string)x.Attribute("contentuid"), x => (string)x);
+                        var translationSentences = translationDoc.Descendants("content")
+                            .ToDictionary(x => (string)x.Attribute("contentuid"), x => (string)x);
+
+                        // 比較翻訳結果を格納
+                        List<TranslationData> langDatasets = new List<TranslationData>();
+
+                        int id = 1;
+                        // uuidをキーに対訳
+                        foreach (var uid in sourceSentences.Keys)
+                        {
+                            if (translationSentences.ContainsKey(uid))
+                            {
+                                string sourceWord = sourceSentences[uid];
+                                string transWord = translationSentences[uid];
+
+                                langDatasets.Add(new TranslationData
+                                {
+                                    UUID = uid,
+                                    SourceLang = sourceWord,
+                                    TransLang = transWord
+                                });
+                                id++;
+                            }
+                        }
+                        InsertDB(DBPath, langDatasets);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"データのインポート中にエラーが発生しました。\n\n{ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InsertDB(string dbPath, List<TranslationData> list)
+        {
+            using (var db = new LiteDatabase(dbPath))
+            {
+                var collection = db.GetCollection<TranslationData>("TranslationData");
+                collection.DeleteAll();
+
+                // DB構築
+                collection.InsertBulk(list);
+                MessageBox.Show($"Creating Dictionary Complete", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
+        
+       
